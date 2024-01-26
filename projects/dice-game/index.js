0d9c0432829a41af6d879e3465dea93a6a8ace27
Sher Ning <sherning@hotmail.com>
2024-01-26 10:38:31 +0000

    my first js game

---
+++ projects/dice-game/index.js
@@ -0,0 +1,23 @@
+// function
+const randNum = () => Math.floor(Math.random() * 5 + 1);
+const getDiceHtml = (num, playerNum) =>
+  `<img class="img${playerNum}" src="images/dice${num}.png"></img>`;
+
+// global
+const titleElement = document.getElementsByTagName("h1")[0];
+const flag = "🚩";
+
+// player 1's number
+const p1Num = randNum();
+const p1Html = ".container .img1";
+
+// player 2's number
+const p2Num = randNum();
+const p2Html = ".container .img2";
+
+document.querySelector(p1Html).outerHTML = getDiceHtml(p1Num, 1);
+document.querySelector(p2Html).outerHTML = getDiceHtml(p2Num, 2);
+
+if (p1Num > p2Num) titleElement.innerHTML = `${flag} Player 1 Win!`;
+else if (p1Num < p2Num) titleElement.innerHTML = `Player 2 Win! ${flag}`;
+else titleElement.innerHTML = `${flag} Draw! ${flag}`;
